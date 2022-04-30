44022bbeac292d0c2a2a97792aabbbc898c834ad
Aman Kumar <aman.asingh307@gmail.com>
2022-05-01 01:45:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aman/healthKit/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.aman.healthKit;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.aman.healthKit.R;
+import com.example.aman.healthKit.home.HomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView mSplashText = (ImageView) findViewById(R.id.splash_logo1);
+        ImageView mSplashLogo = (ImageView) findViewById(R.id.splash_logo);
+        Animation uptodown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        Animation downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+
+        mSplashText.setAnimation(downtoup);
+        mSplashLogo.setAnimation(uptodown);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                HomeActivity.launch(SplashActivity.this);
+                finish();
+            }
+        }, 3000);
+    }
+
+}
---
+++ app/src/main/java/com/example/aman/healthKit/home/SectionPagerAdapter.java
@@ -0,0 +1,60 @@
+package com.example.aman.healthKit.home;
+
+
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.aman.healthKit.home.fragments.DateFragment;
+import com.example.aman.healthKit.home.fragments.DoctorFragment;
+import com.example.aman.healthKit.home.fragments.SpecializationFragment;
+
+
+public class SectionPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                SpecializationFragment specializationFragment = new SpecializationFragment();
+                return specializationFragment;
+
+            case 1:
+                DoctorFragment doctorFragment = new DoctorFragment();
+                return doctorFragment;
+
+            case 2:
+                DateFragment dateFragment = new DateFragment();
+                return dateFragment;
+
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "SPECIALIZATION";
+            case 1:
+                return "DOCTOR";
+            case 2:
+                return "DATE";
+
+            default:
+                return null;
+        }
+    }
+}
